876084d29f8ec0914829f57d41fe905ee47cb147
Angel Agustín Martínez <angel.agustin.martinez@gmail.com>
2015-12-30 13:38:04 -0600

    Initial Commit

---
+++ pizzaWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  pizzaWatch WatchKit Extension
+//
+//  Created by Angel Agustín Martínez on 30/12/15.
+//  Copyright © 2015 Angel Agustín Martínez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ pizzaWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pizzaWatch
+//
+//  Created by Angel Agustín Martínez on 30/12/15.
+//  Copyright © 2015 Angel Agustín Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
